e1a72b018a7b1a622afca244ec2bc851d049f17c
Geoffrey Bergeret <geoffrey@gbergeret.com>
2018-11-02 21:54:22 +0000

    test: Add test and fix existing tests
    Add a test on operating system and fix/refactor existing tests with curl.

--- test/integration/default/controls/internet_access.rb
+++ test/integration/default/controls/internet_access.rb
@@ -1,9 +1,9 @@
 control 'internet_access' do
   describe command("curl -I http://www.google.com") do
-    its('stdout') { should match (/302 Found/) }
+    its('stdout') { should match (/200 OK/) }
   end
 
   describe command("curl -I https://www.google.com") do
-    its('stdout') { should match (/302 Found/) }
+    its('stdout') { should match (/200 OK/) }
   end
 end
